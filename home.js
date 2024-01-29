d009055c5c5b41d9fd6796a37a4e01d07221fe52
23nosurrend <keynesbizimana@gmail.com>
2024-01-29 09:46:15 +0100

    I have added javscript for login

--- home.js
+++ home.js
@@ -17,4 +17,16 @@ function cancelFucntion(){
     let loginStatus=document.getElementById("login-form") 
     loginStatus.style.display=(loginStatus.style.display==="block" ||loginStatus.style.display==="inline")?"none":"none"
 }
+
+// Validation of credintials
+
+function navigate(){
+    let name=document.getElementById("username")
+    let pass=document.getElementById("password")
+
+    if(name.value==="keynesbizimana@gmail.com"&&pass.value===123){
+        alert("welcome")
+    }
+}
+
         
