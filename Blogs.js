2676a0a8b68471454be827575197c2e3918bb580
23nosurrend <keynesbizimana@gmail.com>
2024-02-02 11:19:10 +0100

    I have modified my code to be able to retrieve blogs from locaStorage

--- Blogs.js
+++ Blogs.js
@@ -7,6 +7,15 @@
 document.addEventListener("DOMContentLoaded",function(){
     // let value=document.getElementById("value")
     // value.innerHTML=found
+    const title=localStorage.getItem("title")
+    const head=document.getElementsByClassName("div-with-header")[0]
+    //now create p to display inside div-with header
+    const headerParagraph=document.createElement("h3")
+    headerParagraph.innerHTML=title
+
+    // now append title to its div
+    head.appendChild(headerParagraph)
+
     let found=localStorage.getItem("blogs")
     let setences=found.split('.')
     let container=document.getElementsByClassName("div-with-intro")[0]
--- uploadBlogs.js
+++ uploadBlogs.js
@@ -6,13 +6,36 @@
 function submitBlog(){
     let written =document.getElementById("text").value
     let title=document.getElementById("title").value
-    let date=document.getElementById("date")
-    alert(written)
-    localStorage.clear()
-    let strDate=JSON.stringify(date)
-    let strTitle=document.getElementById("title")
-    let str=JSON.stringify(written)
-    localStorage.setItem("date",strDate)
-    localStorage.setItem("title",strTitle)
-    localStorage.setItem("blogs",str)
+    let date=document.getElementById("date").value
+    let image=document.getElementById("image").files
+    alert (image.length)
+    console.log(image)
+
+    if(image.length>0){
+        const Reader=new FileReader()
+        //reader only the first image
+        Reader.readAsDataURL(image[0])
+        //create onload function
+        Reader.onload=function(event){
+            const imageData=event.target.result
+
+             localStorage.clear()
+
+            let strDate=JSON.stringify(date)
+            let strTitle=JSON.stringify(title)
+            let str=JSON.stringify(written)
+            localStorage.setItem("date",strDate)
+            localStorage.setItem("title",strTitle)
+            localStorage.setItem("blogs",str)
+            localStorage.setItem("picture",imageData)
+
+        }
+
+    }else{
+        alert ("Select image")
+    }
+    
+    
+   
+
 }
