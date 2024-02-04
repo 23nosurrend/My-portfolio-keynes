728d6e24054e6463a3cdf8637560f230dd78ff1c
23nosurrend <keynesbizimana@gmail.com>
2024-02-04 20:03:39 +0100

    Now blogs can be viewd all when displayed

--- Blogs.js
+++ Blogs.js
@@ -2,18 +2,65 @@
 
 document.addEventListener("DOMContentLoaded",function(){
     //retrieve image
-    const image=localStorage.getItem("picture")
+    const found=localStorage.getItem("Blogs")
+    
+    let Data=JSON.parse(found)
+  for(let i=0;i<Data.length;i++){
+    let profile=Data[i].picture
+    alert("Here you can view all blogs")
+
+   
     const father=document.getElementsByClassName("father-div")[0]
 
     const outer=document.createElement("div")
     outer.className="outer-div";
 
+    const divImage=document.createElement("div")
+     divImage.className="div-with-img"
+
     const imageTag=document.createElement("img")
      imageTag.className="img1"
      imageTag.alt="Image of teh story"
-     imageTag.src=image
-     outer.appendChild(imageTag)
+     imageTag.src=profile
+     divImage.appendChild(imageTag)
+     outer.appendChild(divImage)
      father.appendChild(outer)
+
+     // about date
+
+     let date=Data[i].date
+     
+     const divText=document.createElement("div")
+     divText.className="div-with-text"
+     const dateParagraph=document.createElement("p")
+     dateParagraph.innerHTML=date
+     divText.appendChild(dateParagraph)
+     outer.appendChild(divText)
+     // about title
+     let title=Data[i].title
+      const divTitle=document.createElement("div")
+          divTitle.className="div-with-header"
+      const titleText=document.createElement("h3")
+           titleText.innerHTML=title
+       divTitle.appendChild(titleText)
+       outer.appendChild(divTitle)
+
+    // About blog content
+    let Blogcontent=Data[i].content
+    const divContent=document.createElement("div")
+        divContent.className="div-with-intro"
+    const divParagraph=document.createElement("p")
+        divParagraph.innerHTML=Blogcontent
+    divContent.appendChild(divParagraph)
+    outer.appendChild(divContent)
+
+
+
+
+
+
+  }
+   
 
     // const title=localStorage.getItem("title")
     // const head=document.getElementsByClassName("div-with-header")[0]
